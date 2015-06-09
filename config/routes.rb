ecfc6eec321e31f430045dc7e68c18af3bc8c43f
mcn <miriam.neubauer@cdtm.de>
2015-06-09 15:35:43 -0400

    tutorial from beginning to bootstrap4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'sites#index'
+  root 'pages#home'
+
+  get 'imprint' => 'pages#imprint'
+  #same as: get 'pages/imprint'
+  get 'contact' => 'pages#contact'
+  get 'home' => 'pages#home'
 
 end
